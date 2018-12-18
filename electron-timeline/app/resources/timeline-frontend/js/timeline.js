af63029f9d1d6c5c7eaad6b116880aec8d125b52
Leblanc-W <728616841@qq.com>
2018-12-18 23:57:45 +0800

    try electron

---
+++ electron-timeline/app/resources/timeline-frontend/js/timeline.js
@@ -0,0 +1,106 @@
+/**
+ * get新消息方法要写在ready里，防止刷新页面取消数据
+ */
+$(document).ready(function() {
+    $.ajax({
+        "url": "http://47.100.239.92:3000/messages",
+        "type": "GET",
+        "success": function (oRes) {
+            console.log(oRes);
+            // console.log('timestamp' + oRes.res[12].time);
+            // console.log(oRes.res[11].picture);
+            var list = document.getElementById('timelines');
+            var resLen = oRes.res.length;
+            for (var i = resLen - 1; i >= 0; i--) {
+                var user = oRes.res[i].username.toString();
+                var con = oRes.res[i].content.toString();
+                var pic = oRes.res[i].picture.toString();
+                var timestamp = oRes.res[i].time;
+                var new_li = createTimelineItem(user, con, pic, timestamp);
+                var count = ($("li").length);
+                console.log('count' + count);
+                if(count > 4) {
+                    new_li.classList.add('hide');
+                }
+                list.append(new_li);
+            }
+        }
+    });
+    /**
+     * 新增消息button
+     */
+    $('#bt-add').click(function () {
+        if(getCookie() !== "") {
+            if(document.getElementById('edit-part').classList.contains('hide')) {
+                document.getElementById('edit-part').classList.remove('hide');
+            } else {
+                document.getElementById('edit-part').classList.add('hide');
+            }
+        } else {
+            alert("请先登录！");
+            ShowLogin();
+        }
+    });
+
+    /**
+     * 发布新消息button
+     */
+    $("#bt-post-new-msg").click(function() {
+        var content = document.getElementById('edit-area');
+        var user = getCookie();
+        var fileDom = document.getElementById('bt-upload-img');
+        var formData = new FormData();
+        formData.append("username", user);
+        formData.append("content", content.value);
+        if(fileDom.value === "") {
+            formData.append("picture", "");
+        } else {
+            console.log(fileDom.value);
+            var pic = fileDom.files[0];
+            formData.append("picture", pic);
+        }
+        if(content.value) {
+            $.ajax ({
+                "url": "http://47.100.239.92:3000/publish",
+                "data": formData,
+                "dataType": "json",
+                "type": "POST",
+                "contentType": false,
+                "processData": false,
+                "success": function(oRes) {
+                    alert("发布成功~");
+                    window.location.href = "";
+                }
+            });
+
+            //清空消息输入栏
+            content.value = "";
+            document.getElementById('bt-upload-img').value = "";
+        } else {
+            alert('请输入内容再发送~');
+        }
+    });
+
+    /**
+     * 取消发布button
+     */
+    $("#bt-cancel-post").click(function () {
+        document.getElementById('edit-part').classList.add('hide');
+    });
+
+    /**
+     * 刷新button
+     */
+    $("#bt-refresh").click(function () {
+        window.location.href = "";
+    });
+
+    $("#bt-load").click(function(){
+        var list = document.getElementById('timelines').childNodes;
+        for(var a = list.length - 1; a >= 0; a--) {
+            if(list.item(a).classList.contains('hide')) {
+                list.item(a).classList.remove('hide');
+            }
+        }
+    });
+});
---
+++ electron-timeline/app/resources/timeline-frontend/js/util/cookie.js
@@ -0,0 +1,26 @@
+function getCookie() {
+    var name = "user=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i].trim();
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function setCookie(cUser) {
+    var d = new Date();
+    d.setTime(d.getTime() + 24 * 60 * 60 * 1000);
+    var expires = "expires="+d.toGMTString();
+    document.cookie = "user=" + cUser + "; " + expires;
+}
+
+function checkCookie() {
+    if(getCookie() !== "") {
+        document.getElementById('log-status').innerText = getCookie();
+    } else {
+        document.getElementById('log-status').innerText = "登录";
+    }
+}
