eb0b169fd5b0a86f7b691f503910008ddde7118a
Leblanc-W <728616841@qq.com>
2018-12-30 16:01:11 +0800

    加入测试文件夹;新增issues更新日志

--- electron-timeline/app/resources/js/data/timeline.js
+++
@@ -1,5 +0,0 @@
-function getTimeline(successCallback) {
-    var sUrl = sDomain + "mockdata/timelines.json";
-    var sDataType = "application/json";
-    get(sUrl, successCallback, sDataType);
-}
--- electron-timeline/app/resources/js/util/cookie.js
+++ electron-timeline/app/resources/js/util/cookie.js
@@ -1,20 +1,27 @@
-var userCookie = "";
-
 function getCookie() {
-    return userCookie;
+    $.getJSON('electron-timeline/app/resources/js/data/user.json', function (data) {
+        var check = data["username"];
+        console.log(check);
+        return check;
+    })
 }
 
 function setCookie(cUser) {
-    userCookie = cUser;
+    $.getJSON('electron-timeline/app/resources/js/data/user.json', function (data) {
+        data["username"] = cUser;
+    })
 }
 
 function clearCookie() {
-    userCookie = "";
+    $.getJSON('electron-timeline/app/resources/js/data/user.json', function (data) {
+        data["username"] = "";
+    })
 }
+
 
 function checkCookie() {
     if(getCookie() !== "") {
-        document.getElementById('log-status').innerText = userCookie;
+        document.getElementById('log-status').innerText = getCookie();
     } else {
         document.getElementById('log-status').innerText = "登录";
     }
